d93c601fb3a9325e99a1e8f669c4df538791418d
Tyler Roussos <tylerroussos@gmail.com>
2013-09-28 00:17:16 -0400

    Updated the file structure and wrapped up the dataURL class, just needs some phpunit tests.

--- troussos/basis/BasisReceiverInterface.php
+++ troussos/basis/BasisReceiverInterface.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: tylerroussos
- * Date: 9/27/13
- * Time: 10:15 PM
- * To change this template use File | Settings | File Templates.
- */
 
 namespace troussos\basis;
 
--- troussos/basis/User.php
+++ troussos/basis/User.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: tylerroussos
- * Date: 9/27/13
- * Time: 9:50 PM
- * To change this template use File | Settings | File Templates.
- */
 
 namespace troussos\basis;
 
